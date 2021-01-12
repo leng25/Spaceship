a74e73b1e9992302853e1af1e3883fe9b7034c14
luisn <lenegringarcia@gmail.com>
2021-01-12 09:09:57 -0500

    Final

---
+++ Assets/change to firs scene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changetofirsscene : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+           		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        change();
+
+   }
+
+    public void change()
+    {
+        Invoke("LoadFirstScene", 8f);
+    }
+
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/change.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change : MonoBehaviour
+{
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Use this for initialization
+
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        changeScene();
+
+    }
+    public void changeScene()
+    {
+        Invoke("LoadFirstScene", 8f);
+    }
+
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+       
+    }
+
+}
